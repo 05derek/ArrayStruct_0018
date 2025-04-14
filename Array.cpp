331cbd6c1ec2a67aefe1fc684a716e2933ebd990
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-04-14 11:03:04 +0700

    memberi nilai array

--- Array.cpp
+++ Array.cpp
@@ -1,10 +1,25 @@
 
+int main(){
+        cout << "Nama pada index 2 adalah " << nama[2] << endl;
+        //memberi nilai pada elemen array per index
+        nama[2]= "derek";
+        cout << "Nama pada index 2 adalah " << nama[2] << endl;
 
-//mendeklarasikan aray tanpa ukuran, wajib diberi nilai awal
-int nilai[]={1,2,3};
+        cout << "Input score" << endl;
+        cout << endl;
 
-//mendeklarasikan array dengan ukuran dan diberi nilai awal
-string nama[5]={"ben", "agus", "fara", "ama", "akila"};
+        //memberi nilai array dengan looping for
+        for(int i=0; i<5; i++){
+            cout << "Score ke- " << i+1 << "=";
+            cin >> score[i];
+        }
 
-//mendeklarasikan array dengan ukuran dan tanpa nilai awal
-float score[5];
+        cout << "Tampilkan score" << endl;
+        cout << endl;
+        
+        //menampilkan nilai array dengan looping for
+        for(int i=0; i<5; i++){
+            cout << "Score ke- " << i+1 << "=" << score[i] << endl;
+        }
+}
+
